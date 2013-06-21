71ee2dfd338a16835b28608237e160b67cbec439
Victor Hung <vhung@kth.se>
2013-06-22 00:56:58 +0200

    Added Currencies.txt and reates.txt

--- TravelDiary/src/kth/vhung/traveldiary/DetailFragment.java
+++ TravelDiary/src/kth/vhung/traveldiary/DetailFragment.java
@@ -7,6 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class DetailFragment {
+public class DetailFragment extends Fragment{
+	
+	@Override
+	  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	      Bundle savedInstanceState) {
+	    View view = inflater.inflate(R.layout.detail_fragment,
+	        container, false);
+	    return view;
+	  }
 
+	  public void setText(String item) {
+	    TextView view = (TextView) getView().findViewById(R.id.detailsText);
+	    view.setText(item);
+	  }
+	
 }
