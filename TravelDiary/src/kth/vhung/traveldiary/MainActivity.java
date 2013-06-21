ea450280e1fed8057cdffc323b0357c493fed405
vhung <victor.hung@outlook.com>
2013-06-21 13:45:41 +0200

    testing

--- TravelDiary/src/kth/vhung/traveldiary/MainActivity.java
+++ TravelDiary/src/kth/vhung/traveldiary/MainActivity.java
@@ -11,31 +11,29 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void go(View view) {
-    	ConnectivityManager connMgr = (ConnectivityManager) 
-    	        getSystemService(Context.CONNECTIVITY_SERVICE);
-    	    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-    	    if (networkInfo != null && networkInfo.isConnected()) {
-    	        
-    	    	
-    	    	
-    	    } else {
-    	    	Context context = getApplicationContext();
-    	    	Toast.makeText(context, "No availible network found", Toast.LENGTH_SHORT).show();
-    	    }
-    	
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void go(View view) {
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+			// Hello
+		} else {
+			Context context = getApplicationContext();
+			Toast.makeText(context, "No availible network found",
+					Toast.LENGTH_SHORT).show();
+		}
+
+	}
 }
